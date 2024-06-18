fceb7ef10569997c711e4f4935dab0990bf4f017
mwangianthony <anthonymwangi627@gmail.com>
2024-06-18 20:12:16 +0300

    added JwtService services class

---
+++ src/main/java/com/mwangianthony/backend/dev/services/impl/JwtServices.java
@@ -0,0 +1,29 @@
+package com.mwangianthony.backend.dev.services.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtServices {
+
+	public String extractUsername(String jwt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String extractClaim(String jwt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String generateToken(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public String extractClaims(String jwt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	 
+
+}
