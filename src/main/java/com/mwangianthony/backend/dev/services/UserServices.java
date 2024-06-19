b6d999eec111158d7870d9ffabdf86b42e3c499b
mwangianthony <anthonymwangi627@gmail.com>
2024-06-19 21:48:32 +0300

    created interface UserServices

---
+++ src/main/java/com/mwangianthony/backend/dev/services/UserServices.java
@@ -0,0 +1,8 @@
+package com.mwangianthony.backend.dev.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserServices {
+
+	UserDetailsService userDetailsServices();
+}
