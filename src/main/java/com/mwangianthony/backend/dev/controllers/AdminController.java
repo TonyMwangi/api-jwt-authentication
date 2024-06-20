800df1d25b1f4968346a793451ed9952cfb94ad1
mwangianthony <anthonymwangi627@gmail.com>
2024-06-20 20:23:56 +0300

    created the controller package to hold all my controller classes

---
+++ src/main/java/com/mwangianthony/backend/dev/controllers/AdminController.java
@@ -0,0 +1,13 @@
+package com.mwangianthony.backend.dev.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/admin")
+public class AdminController {
+
+}
